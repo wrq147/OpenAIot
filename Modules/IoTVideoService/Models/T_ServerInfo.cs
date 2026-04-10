31e935ad569dd5c32514fb97338d2c7f079e0daa
hello <world>
2026-04-10 15:44:47 +0800

    修改检测

---
+++ Modules/IoTVideoService/Models/T_ServerInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTVideoService.Models
+{
+    public class T_ServerInfo
+    {
+        public string NodeId { get; set; }
+        public string Ip { get; set; }
+        public int RtmpPort { get; set; }
+        public int HttpPort { get; set; }
+        public DateTime Expire { get; set; }
+    }
+}
