b754b928d1a9cee38d685f002db4fc033a182ce3
hello <world>
2026-06-05 17:33:08 +0800

    优化ai助手2

---
+++ Modules/LLMService/Tool/LongMemory.cs
@@ -0,0 +1,27 @@
+﻿using LLMService.Business;
+using Microsoft.Extensions.AI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateAction.Core;
+
+namespace LLMService.Tool
+{
+    /// <summary>
+    /// 长期记忆工具
+    /// </summary>
+    public static class LongMemory
+    {
+        public static AITool CreateSearchRelatedMemoriesTool(ITAServiceProvider provider)
+        {
+            return AIFunctionFactory.Create(
+              provider.GetService<MemoryRagBLL>().SearchRelatedMemoriesAsync,
+              name: "SearchRelatedMemoriesAsync",
+              description: "根据用户问题搜索语义相关的历史聊天记忆"
+            );
+        }
+    }
+}
