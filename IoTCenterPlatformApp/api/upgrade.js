4ea0a35c1749754211cc9ff701f3759e3dc5c2df
wrq147 <53035636+wrq147@users.noreply.github.com>
2026-08-05 16:47:34 +0800

    Delete IoTCenterPlatformApp directory

--- IoTCenterPlatformApp/api/upgrade.js
+++
@@ -1,9 +0,0 @@
-import request from '@/common/request.js'
-
-export function checkVersion(appVersion,wgtVersion,orgId,styleId) {
-	// 'orgId':orgId,
-	return request.get({
-		url: '/AuthService/Upgrade/CheckVersion',
-		query: {"appVersion":appVersion,"wgtVersion":wgtVersion,'styleId':styleId}
-	});
-}
--- IoTCenterPlatformApp/common/js/phone-info.js
+++
@@ -1,5 +0,0 @@
-export default {
-    systemInfo: {}, // 系统设备信息
-    manifestInfo: "" || uni.getStorageSync("widgetInfo"), // manifest.json 应用信息
-}
-
